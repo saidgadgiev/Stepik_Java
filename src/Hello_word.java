ca99b790bb02792fde3a3da432f878f4fd150bc0
Said Gadjiev <ackusha1984@ya.ru>
2023-04-01 00:33:19 +0300

    day 01042023

---
+++ src/Hello_word.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Hello_word {
+
+    public static void main(String[] args) {
+//        System.out.print("Hello world ");
+//        System.out.println("Hello world");
+//        System.out.println("Mother" + " Son " + "Father");
+        int i=7;
+        int j=5;
+//        System.out.println("i="+i); //вывод i=7
+//        System.out.println("i+j="+i+j); //вывод i+j=75
+//        System.out.println("i+j="+(i+j)); //вывод i+j=12
+//        System.out.println(i+j); //вывод 12
+//        System.out.printf("i=%d j=%d\n",i,j);
+//        System.out.printf("i=%-5d k=%5d\n",i,j);//i выравнивается влево в 5 позициях, k - вправо
+//        int rub=3,kop=0;
+//        System.out.printf("%03d руб. %04d коп.",rub,kop);//выводится: 03 руб. 00 коп.
+//        double x=3.123456;
+//        System.out.printf("x=%6.3f\n",x);//число выводится в 8 позициях
+//и округляется до 2 знаков после запятой
+        // Ввод с консоли
+//        String name;
+        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter yor name and click Enter: ");
+//        name = scan.next();// Read line befor the gap
+//        System.out.println("Hello, "+name);
+//        int age;
+//        System.out.println("Enter you age: ");
+//        age = scan.nextInt();// Read line befor the gap and convert int
+//        System.out.println("Your age - "+ age);
+
+//hasNextInt() позволяет проверить,
+// что следующий элемент потока ввода
+// может быть преобразован в целое число.
+        int age;
+        System.out.println("Введите свой возраст: ");
+        if(scan.hasNextInt()) {//проверка того, что далее в потоке целое число
+            age = scan.nextInt(); //Читает строку до пробела и преобразует в int
+            System.out.println("Ваш возраст - " + age);
+        }else{
+            System.out.println("Неверные данные о возрасте!");
+        }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        String name = "Андрей";
+        int age = 23;
+        double discount = 15.5;
+        // put your code here
+        System.out.printf("С Днем рождения, " + name + "!%n");
+        System.out.printf("Сегодня Вам "+age+"!%n");
+        System.out.printf("По этому поводу Вам скидка "+discount+"%%%n");
+        System.out.printf("на весь ассортимент нашего сайта!");
+    }
+}
